883caba3f847521fdb0fe0fcefdacf78d4b1895d
developer-mahin <mdmahin1310@gmail.com>
2022-08-30 23:02:36 +0600

    final commit

--- phone-hunter/js/script.js
+++ phone-hunter/js/script.js
@@ -24,7 +24,7 @@ const displayPhoneData = (phones, dataLimit) => {
         phones = phones.slice(0, 10);
         showAllContainer.classList.remove('d-none')
     }
-    else{
+    else {
         showAllContainer.classList.add('d-none')
     }
 
@@ -42,12 +42,13 @@ const displayPhoneData = (phones, dataLimit) => {
         const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML = `
-            <div class="card p-4 border">
+            <div class="card pt-3 border">
                 <div>
-                    <img src="${image}" class="card-img-top " alt="...">
+                    <img src="${image}" class="card-img-top w-75" alt="...">
                 </div>
                 <div class="card-body">
-                    <h5 class="card-title">Phone: ${phone_name}</h5>
+                    <h6 class="card-title">Phone: ${phone_name}</h6>
+                    <button onclick="showMoreDetails('${slug}')" href="#" class="btn btn-outline btn-outline-success fw-semibold" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
                 </div>
             </div>
         `;
@@ -57,23 +58,29 @@ const displayPhoneData = (phones, dataLimit) => {
     togglerSpinner(false);
 }
 
+
 // common function for search and show all button
 const processSearch = (dataLimit) => {
     // start loader spinner
     togglerSpinner(true);
     const searchInput = document.getElementById('search-field');
     const searchInputValue = searchInput.value;
-    searchInput.value = "";
+
     phoneData(searchInputValue, dataLimit);
 }
 
 // handle search button click
-document.getElementById('search-button').addEventListener('click', function(){
+document.getElementById('search-button').addEventListener('click', function () {
     processSearch(10);
 })
-/* const searchPhone = () => {
-    processSearch(10);
-} */
+
+// input field enter key event
+document.getElementById('search-field').addEventListener('keyup', function (event) {
+    if (event.key === "Enter") {
+        processSearch(10);
+    }
+})
+
 
 // spinner loader
 const togglerSpinner = (isLoading) => {
@@ -87,9 +94,29 @@ const togglerSpinner = (isLoading) => {
 }
 
 // show all button click handler 
-document.getElementById('show-all-button').addEventListener('click', function(){
+document.getElementById('show-all-button').addEventListener('click', function () {
     processSearch();
 })
 
+// show more details function 
+const showMoreDetails = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayMoreDetails(data.data);
+}
 
+const displayMoreDetails = (moreDetails) => {
+    console.log(moreDetails);
+    const phoneImage = document.getElementById('phone-image');
+    phoneImage.src = moreDetails.image;
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = moreDetails.name;
+    const mainFeatures = document.getElementById('mainFeatures');
+    mainFeatures.innerHTML = `
+        <p>ChipSet: <span>${moreDetails.mainFeatures.chipSet}</span></p> 
+        <p>Display Size: <span>${moreDetails.mainFeatures.displaySize}</span></p>
+        <p>Memory: <span>${moreDetails.mainFeatures.memory}</span></p>
+    `;
+}
 phoneData('iphone');
